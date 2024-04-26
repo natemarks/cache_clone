63411167d4523db967b180864c7318cc314946a0
Nate Marks <nmarks@imprivata.com>
2024-04-26 17:55:55 -0400

    sdf

--- cmd/clone.go
+++ cmd/clone.go
@@ -6,19 +6,32 @@ package cmd
 
 import (
 	"github.com/natemarks/cache_clone/config"
+	"github.com/natemarks/cache_clone/internal/aws"
+	"github.com/natemarks/cache_clone/internal/git"
 	"github.com/spf13/cobra"
 )
 
 // cloneCmd represents the clone command
 var cloneCmd = &cobra.Command{
 	Use:   "clone",
-	Short: "CLone a remote repo to a local directory using a local mirror",
+	Short: "Clone a remote repo to a local directory using a local mirror",
 	Long: `Access the stash credentials from AWS Secret Manager. 
                      Create or update a local mirror of the repo.
                      Clone using the local mirror`,
 	Run: func(cmd *cobra.Command, args []string) {
 		log := config.GetLogger(settings)
-		log.Info().Msg("Clone command")
+		// Get credentials from AWS Secret Manager
+		creds, err := aws.GetRemoteCredentials(aws.GetRemoteCredentialsInput{
+			AWSSMSecretID: settings.SecretID,
+			UsernameKey:   settings.UserKey,
+			TokenKey:      settings.TokenKey,
+		}, &log)
+		log.Info().Msg("Try to Clone Repo")
+		err = git.GetMirror(settings.Remote, settings.Mirror, settings.Local, creds.Username, creds.Token, &log)
+		if err != nil {
+			log.Fatal().Err(err)
+		}
+		log.Info().Msg("Successfully cloned the repo")
 	},
 }
 
